49b291f7fc177ae39e088eabe66c8ba2fdd0141b
Bruno Sena Saunders <senabruno97@hotmail.com>
2022-07-06 16:57:23 -0300

    declarando as funções antes de serem executadas

--- main.py
+++ main.py
@@ -3,96 +3,6 @@ from models import *
 from crud import *
 
 
-def menu_comandos():
-    print("\nDigite: ")
-    print(" 1 --> Cadastrar uma peça")
-    print(" 2 --> Alterar uma peça")
-    print(" 3 --> Remover uma peça")
-    print(" 4 --> Cadastrar um estilo")
-    print(" 5 --> Alterar um estilo")
-    print(" 6 --> Remover um estilo")
-    print(" 7 --> Listar todas as peças")
-    print(" 8 --> Listar peças por tamanho e padrão")
-    print(" 9 --> Listar estilos")
-    print("10 --> Pesquisar estilo por nome")
-    print("11 --> Listar peças para venda")
-    print("12 --> Listar peças para doação")
-    print("13 --> Vender uma peça")
-    print("14 --> Doar uma peça")
-    print("15 --> Listar peças vendidas")
-    print("16 --> Listar peças doadas")
-    print("17 --> Salvar alterações")
-    print("18 --> Carregar arquivos")
-    print(" 0 --> Finalizar programa")
-    
-
-def interface_usuario():
-    comando = -1
-
-    print("\n\n---------- Bem vindo ao Guarda-Roupa Virtual ----------")
-    while comando != 0:
-        menu_comandos()
-
-        try:
-            comando = int(input("\nO que deseja fazer? "))
-        except:
-            print("\n\nATENÇÃO: Digite apenas um número do menu")
-            continue
-
-        # Pega o comando do usuário e o executa
-        if comando == 1:
-            comando1()
-        elif comando == 2:
-            comando2()
-        elif comando == 3:
-            comando3()
-        elif comando == 4:
-            comando4()
-        elif comando == 5:
-            comando5()
-        elif comando == 6:
-            comando6()
-        elif comando == 7:
-            comando7()
-        elif comando == 8:
-            comando8()
-        elif comando == 9:
-            comando9()
-        elif comando == 10:
-            comando10()
-        elif comando == 11:
-            comando11()
-        elif comando == 12:
-            comando12()
-        elif comando == 13:
-            comando13()
-        elif comando == 14:
-            comando14()
-        elif comando == 15:
-            comando15()
-        elif comando == 16:
-            comando16()
-        elif comando == 17:
-            comando17()
-        elif comando == 18:
-            comando18()
-        elif comando == 0:
-            comando0()
-        else:
-            print("ATENÇÃO: Digite apenas um número do menu")
-        
-    # Saiu do while, acabou o programa
-    print("\nGuarda-Roupa Virtual encerrado")
-
-    
-def main():
-    interface_usuario()
-
-
-if __name__ == "__main__":
-    main()
-
-
 # ATENÇÃO
 # Implementar os comandos usando as funções já criadas. Testar tudo a fim de encontrar possíveis bugs.
 # Se encontrarem algum bug, tentem resolver e alertem sobre ele no grupo.
@@ -191,3 +101,93 @@ def comando18():
 # Finalizar programa - Bruno
 def comando0():
     return
+
+
+def menu_comandos():
+    print("\nDigite: ")
+    print(" 1 --> Cadastrar uma peça")
+    print(" 2 --> Alterar uma peça")
+    print(" 3 --> Remover uma peça")
+    print(" 4 --> Cadastrar um estilo")
+    print(" 5 --> Alterar um estilo")
+    print(" 6 --> Remover um estilo")
+    print(" 7 --> Listar todas as peças")
+    print(" 8 --> Listar peças por tamanho e padrão")
+    print(" 9 --> Listar estilos")
+    print("10 --> Pesquisar estilo por nome")
+    print("11 --> Listar peças para venda")
+    print("12 --> Listar peças para doação")
+    print("13 --> Vender uma peça")
+    print("14 --> Doar uma peça")
+    print("15 --> Listar peças vendidas")
+    print("16 --> Listar peças doadas")
+    print("17 --> Salvar alterações")
+    print("18 --> Carregar arquivos")
+    print(" 0 --> Finalizar programa")
+    
+
+def interface_usuario():
+    comando = -1
+
+    print("\n\n---------- Bem vindo ao Guarda-Roupa Virtual ----------")
+    while comando != 0:
+        menu_comandos()
+
+        try:
+            comando = int(input("\nO que deseja fazer? "))
+        except:
+            print("\n\nATENÇÃO: Digite apenas um número do menu")
+            continue
+
+        # Pega o comando do usuário e o executa
+        if comando == 1:
+            comando1()
+        elif comando == 2:
+            comando2()
+        elif comando == 3:
+            comando3()
+        elif comando == 4:
+            comando4()
+        elif comando == 5:
+            comando5()
+        elif comando == 6:
+            comando6()
+        elif comando == 7:
+            comando7()
+        elif comando == 8:
+            comando8()
+        elif comando == 9:
+            comando9()
+        elif comando == 10:
+            comando10()
+        elif comando == 11:
+            comando11()
+        elif comando == 12:
+            comando12()
+        elif comando == 13:
+            comando13()
+        elif comando == 14:
+            comando14()
+        elif comando == 15:
+            comando15()
+        elif comando == 16:
+            comando16()
+        elif comando == 17:
+            comando17()
+        elif comando == 18:
+            comando18()
+        elif comando == 0:
+            comando0()
+        else:
+            print("ATENÇÃO: Digite apenas um número do menu")
+        
+    # Saiu do while, acabou o programa
+    print("\nGuarda-Roupa Virtual encerrado")
+
+    
+def main():
+    interface_usuario()
+
+
+if __name__ == "__main__":
+    main()
